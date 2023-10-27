36cf94be8d76e4f9a871fe8818e77a72565982d2
tujunjie <tjj747487547@sina.com>
2023-10-27 16:33:13 +0800

    HELP.md

---
+++ src/test/java/com/example/spring_mobile_management_backend/SpringMobileManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mobile_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMobileManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
