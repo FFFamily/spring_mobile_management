15fd9dfcf9351769bd81b98b1f72e2b30292ac18
tujunjie <tjj747487547@sina.com>
2023-12-11 22:33:29 +0800

    应收应付生成

--- service_policy/src/main/java/org/example/service/PolicyService.java
+++ service_policy/src/main/java/org/example/service/PolicyService.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.example.core.policy.PolicyDto;
 import org.example.entity.CommonException;
-
 import org.example.entity.Policy;
 import org.example.entity.PromotionItem;
 import org.example.mapper.PolicyMapper;
@@ -29,15 +28,15 @@ public class PolicyService {
      * 创建 Policy
      */
     @Transactional(rollbackFor = Exception.class)
-    public void createPolicy(PolicyDto policyDto){
+    public void createPolicy(PolicyDto policyDto) {
         Policy oldPolicy = policyMapper.selectOne(new LambdaQueryWrapper<Policy>()
                 .eq(Policy::getNo, policyDto.getNo()));
-        if (oldPolicy != null){
+        if (oldPolicy != null) {
             throw new CommonException("重复的policyNo");
         }
         Policy policy = new Policy();
 
-        BeanUtils.copyProperties(policyDto,policy);
+        BeanUtils.copyProperties(policyDto, policy);
         policyMapper.insert(policy);
         List<PromotionItem> list = policyDto.getPromotions().stream().map(item -> {
             PromotionItem promotionItem = new PromotionItem();
@@ -50,13 +49,14 @@ public class PolicyService {
             return promotionItem;
         }).toList();
         promotionItemService.insertList(list);
-        rabbitTemplate.convertAndSend("RootDirectExchange","RootDirectRouting", JSON.toJSONString(policyDto));
+        policyDto.setId(policy.getId());
+        rabbitTemplate.convertAndSend("RootDirectExchange", "RootDirectRouting", JSON.toJSONString(policyDto));
     }
 
     public PolicyDto findPolicyById(String policyId) {
         PolicyDto policyDto = policyMapper.findById(policyId);
-        if (policyDto == null){
-            throw new CommonException("未能查询到对应的保单："+policyId);
+        if (policyDto == null) {
+            throw new CommonException("未能查询到对应的保单：" + policyId);
         }
         return policyDto;
     }
